779e390edee4ac9c744fc567419ff4a2aa5ff304
tongbin102 <tongbin102@126.com>
2021-03-10 10:57:34 +0800

    1

---
+++ src/main/java/com/tb/hc/util/HCNetSDKUtils.java
@@ -0,0 +1,47 @@
+package com.tb.hc.util;
+
+import com.sun.jna.NativeLong;
+import com.tb.hc.sdk.HCNetSDK;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author bin.tong
+ * @since 2021/3/10 10:56
+ **/
+@Slf4j
+public class HCNetSDKUtils {
+
+  private static HCNetSDK hCNetSDK = HCNetSDK.INSTANCE;
+
+  String host = "172.4.1.170";
+  String port = "8000";
+  String username = "admin";
+  String password = "123456";
+
+  public void init(){
+    boolean initResult = hCNetSDK.NET_DVR_Init();
+    if(!initResult){
+      log.error("初始化失败！");
+    }
+
+  }
+
+  public static void main(String[] args) {
+    HCNetSDKUtils sdkUtils = new HCNetSDKUtils();
+    sdkUtils.init();
+    hCNetSDK.NET_DVR_SetConnectTime(2000,1);
+    hCNetSDK.NET_DVR_SetReconnect(10000,true);
+
+    sdkUtils.login();
+  }
+
+  public void login(){
+    HCNetSDK.NET_DVR_DEVICEINFO_V30 dvrDeviceinfo =  new HCNetSDK.NET_DVR_DEVICEINFO_V30();
+    NativeLong userId = hCNetSDK.NET_DVR_Login_V30(host,Short.parseShort(port),username, password, dvrDeviceinfo);
+    if (userId.longValue() == -1) {
+      log.error("注册失败！", hCNetSDK.NET_DVR_GetLastError());
+      hCNetSDK.NET_DVR_Cleanup();
+      return;
+    }
+  }
+}
