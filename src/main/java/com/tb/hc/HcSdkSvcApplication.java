44494b73974905e736325b7cd519378cd96cb1cf
tongbin102 <tongbin102@126.com>
2021-03-10 10:50:16 +0800

    add maven struct add pom.xml add hc lib

---
+++ src/main/java/com/tb/hc/HcSdkSvcApplication.java
@@ -0,0 +1,13 @@
+package com.tb.hc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcSdkSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcSdkSvcApplication.class, args);
+    }
+
+}
